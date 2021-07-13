f24c59d11bbaab75fabd8c59d6b73dcf362c0ecc
nomono10 <85695689+nomono10@users.noreply.github.com>
2021-07-13 16:40:01 +0200

    Add files via upload

---
+++ da1.py
@@ -0,0 +1,2 @@
+import base64
+exec(base64.b64decode('cHJpbnQoJyAgIFNjcmlwdCBpcyBsb2NrZWQgZm9yIE1haW50YWluYW5jZSAnKydcbicpCnByaW50KCcgICBGb3IgbmV3IHNjcmlwdHMgam9pbiBvdXIgQ2hhbm5lbCAnKydcbicpCnByaW50KCcgICAgIGh0dHBzOi8vdC5tZS92aXBfbmV0MTAgJyk='))
